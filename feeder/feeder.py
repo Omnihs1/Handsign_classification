b594a5dacbf1b2327e92e5f69f8d4f1f5195e15c
Omnihs1 <antandbee89@gmail.com>
2023-10-09 21:38:48 +0700

    Fix feeder
    delete last dimension

--- feeder/feeder.py
+++ feeder/feeder.py
@@ -3,14 +3,31 @@ import torch.nn as nn
 from torch.utils.data import Dataset
 from pathlib import Path
 import numpy as np
-
+import pickle 
 # Class read npy and pickle file to make data and label in couple
 class FeederINCLUDE(Dataset):
+    """ Feeder for skeleton-based action recognition
+    Arguments:
+        data_path: the path to '.npy' data, the shape of data should be (N, C, T, V, M)
+        label_path: the path to label
+        window_size: The length of the output sequence
+    """
     def __init__(self, data_path: Path, label_path: Path):
         super(FeederINCLUDE, self).__init__
         self.data_path = data_path
         self.label_path = label_path
-        self.data = np.load(self.data_path)
+        self.load_data()
+    
+    def load_data(self):
+        # data: N C V T M
+
+        # load label
+        with open(self.label_path, 'rb') as f:
+            self.sample_name, self.label = pickle.load(f)
+
+        # load data
+        self.data = np.load(self.data_path)     
+        self.N, self.C, self.T, self.V, self.M = self.data.shape
     def __getitem__(self, index):
         """
         Input shape (N, C, V, T, M)
@@ -24,11 +41,10 @@ class FeederINCLUDE(Dataset):
         C : numbers of features
         V : numbers of joints (as nodes)
         T : numbers of frames
-        M : numbers of people (should delete)
         label : label of videos
         """
-        data = self.data_path[index]
-        label = self.self.label_path[index]
+        data = np.squeeze(np.array(self.data_path[index]))
+        label = self.label[index]
         return data, label
     def __len__(self):
         return len(self.label_path)
